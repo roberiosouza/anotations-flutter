dedcb073b2f90654ca4bcd67eca2393278c7b2ab
Robério <rroberiosouza@gmail.com>
2024-06-27 22:59:56 -0300

    Implementado interface básica do App

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,8 +8,63 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _tituloController = TextEditingController();
+  TextEditingController _descricaoController = TextEditingController();
+  _exibirTelaDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Adicionar anotação"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _tituloController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      labelText: "Título", hintText: "Digite o título"),
+                ),
+                TextField(
+                  controller: _descricaoController,
+                  decoration: InputDecoration(
+                      labelText: "Descrição", hintText: "Digite a descrição"),
+                )
+              ],
+            ),
+            actions: [
+              ElevatedButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text("Cancelar")),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Salvar"))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Minhas anotações",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightGreen,
+        foregroundColor: Colors.white,
+        child: Icon(Icons.add),
+        onPressed: () {
+          _exibirTelaDialog();
+        },
+      ),
+    );
   }
 }
